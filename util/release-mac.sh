36522e7453d8581529bc0b0f00a79a5fe31c62a8
alanlu <alanlu2468@gmail.com>
2024-09-21 14:42:22 +0930

    Update release script

---
+++ util/release-mac.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+version=$1
+
+cd target
+
+rm -rf apps
+mkdir apps
+
+cd release
+rm -rf app_out
+ninja app
+mv app_out/block.app "../apps/block-macos-amd64-v${version}.app"
+
+cd ../macosx-arm64-release
+rm -rf app_out
+ninja app
+mv app_out/block.app "../apps/block-macos-arm64-v${version}.app"
+
+cd ..
+cd apps
+
+zip -rq "block-macos-amd64-v${version}.zip" "block-macos-amd64-v${version}.app"
+zip -rq "block-macos-arm64-v${version}.zip" "block-macos-arm64-v${version}.app"
+
+rm -rf "block-macos-amd64-v${version}.app"
+rm -rf "block-macos-arm64-v${version}.app"
+
+open .
